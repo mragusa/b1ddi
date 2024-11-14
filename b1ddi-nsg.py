80b15c6e2af0fb0e510620288e498f902fa03c09
Michael Ragusa <mragusa@users.noreply.github.com>
2024-11-14 13:13:44 -0500

    added create and delete functionality. Create only adds niosx hosts.

--- b1ddi-nsg.py
+++ b1ddi-nsg.py
@@ -10,10 +10,13 @@ import click
 @click.option("-c", "--config", default="b1config.ini", help="BloxOne Configuration File")
 @click.option("-g", "--get", is_flag=True, default=False, help="Retreive BloxOne Hosts and AuthNSG")
 @click.option("-n", "--new", is_flag=True, default=False, help="Create BloxOne AuthNSG")
+@click.option("-d", "--delete", is_flag=True, default=False, help="Delete BloxOne AuthNSG")
 @click.option("--host", multiple=True, default=[], help="BloxOne Host")
-@click.option("--name", help="NSG Name")
+@click.option("--ansgname", help="NSG Name")
+@click.option("--comment", help="NSG Comment")
+@click.option("--ansgid", help="Auth NSG ID")
 
-def main(config, get, new):
+def main(config, get, new, delete, host, ansgname, comment, ansgid):
     b1ddi = bloxone.b1ddi(config)
     if get:
         # Display available hosts
@@ -22,7 +25,9 @@ def main(config, get, new):
         get_auth_nsg(b1ddi)
     # Create NSG
     if new:
-        create_auth_nsg(b1ddii, host)
+        create_auth_nsg(b1ddi, host, ansgname, comment)
+    if delete:
+        delete_auth_nsg(b1ddi, ansgid)
 
 def get_dns_hosts(b1ddi):
     table = PrettyTable()
@@ -50,11 +55,22 @@ def get_auth_nsg(b1ddi):
     else:
         print(response.status_code, response.text)
 
-def create_auth_nsg(b1ddi, host):
-    ansgBody = {"comment": comment, "internal_secondaries": host}
+def create_auth_nsg(b1ddi, host, ansgname, comment):
+    niosx_hosts = []
+    for nxh in host:
+        niosx_hosts.append({"host": nxh})
+    ansgBody = {"comment": comment, "internal_secondaries": niosx_hosts, "name": ansgname}
     response = b1ddi.create("/dns/auth_nsg", body=json.dumps(ansgBody))
     if response.status_code == 200:
-        print(response.json)
+        nsg_response = response.json()
+        print(nsg_response["result"])
+    else:
+        print(response.status_code, response.text)
+
+def delete_auth_nsg(b1ddi, ansgid):
+    response = b1ddi.delete("/dns/auth_nsg", id=ansgid)
+    if response.status_code == 200:
+        print("{} has been deleted successfully".format(ansgid))
     else:
         print(response.status_code, response.text)
 
