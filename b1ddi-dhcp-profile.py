4edd765b30ff523dbb26f91b474fee3f5a3aa207
Michael Ragusa <mragusa@users.noreply.github.com>
2024-11-16 13:06:32 -0500

    applied style

--- b1ddi-dhcp-profile.py
+++ b1ddi-dhcp-profile.py
@@ -7,6 +7,7 @@ from click_option_group import optgroup
 from rich.console import Console
 from rich.table import Table
 
+
 @click.command()
 @optgroup.group("BloxOne Configuration File")
 @optgroup.option("-c", "--config", default="b1config.ini", help="BloxOne Ini File")
@@ -19,7 +20,6 @@ from rich.table import Table
 @optgroup.option("--comment", help="Profile Description")
 @optgroup.group("Delete DHCP Profile Options")
 @optgroup.option("--id", help="Profile ID")
-
 def main(config, get, new, delete, name, comment, id):
     b1ddi = bloxone.b1ddi(config)
     if get:
@@ -29,11 +29,14 @@ def main(config, get, new, delete, name, comment, id):
     if delete:
         del_dhcp_config(b1ddi, id)
 
+
 def get_dhcp_global(b1ddi):
     response = b1ddi.get("/dhcp/server")
     if response.status_code == 200:
         dhcpGlobal = response.json()
-        table = Table("Created", "Name", "Comment", "ID", title="BloxOne DHCP Configurations")
+        table = Table(
+            "Created", "Name", "Comment", "ID", title="BloxOne DHCP Configurations"
+        )
         for x in dhcpGlobal["results"]:
             table.add_row(x["created_at"], x["name"], x["comment"], x["id"])
         console = Console()
@@ -41,13 +44,15 @@ def get_dhcp_global(b1ddi):
     else:
         print(response.status_code, response.text)
 
+
 def create_dhcp_config(b1ddi, name, comment):
-    dhBody= {"name": name, "comment": comment}
+    dhBody = {"name": name, "comment": comment}
     response = b1ddi.create("/dhcp/server", body=json.dumps(dhBody))
     if response.status_code == 200:
         get_dhcp_global(b1ddi)
     else:
         print(response.status_code, response.text)
+
 
 def del_dhcp_config(b1ddi, id):
     response = b1ddi.delete("/dhcp/server", id=id)
