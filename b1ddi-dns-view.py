a0cf33537ef55136d82c988c40d37554791fa60e
Michael Ragusa <mragusa@users.noreply.github.com>
2024-11-16 13:57:20 -0500

    added create and delete functions

--- b1ddi-dns-view.py
+++ b1ddi-dns-view.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import bloxone
+import json
 from rich.console import Console
 from rich.table import Table
 import click
@@ -13,10 +14,26 @@ from click_option_group import optgroup
 @optgroup.group("BloxOne DNS Views")
 @optgroup.option("-g", "--get", is_flag=True, help="Retreive Current DNS Views")
 @optgroup.option("-n", "--new", is_flag=True, help="Create New DNS View")
-def main(config, get, new):
+@optgroup.option("-d", "--delete", is_flag=True, help="Delete DNS View")
+@optgroup.group("New View Options")
+@optgroup.option(
+    "--ipspace",
+    multiple=True,
+    default=[],
+    help="IP Space to Assign to DNS View: Requires full ID path",
+)
+@optgroup.option("--name", help="DNS View Name")
+@optgroup.option("--comment", help="DNS View Comment")
+@optgroup.group("Delete View Options")
+@optgroup.option("--id", help="DNS View ID")
+def main(config, get, new, delete, ipspace, name, comment, id):
     b1ddi = bloxone.b1ddi("b1config.ini")
     if get:
         get_dns_view(b1ddi)
+    if new:
+        create_dns_view(b1ddi, name, comment, ipspace)
+    if delete:
+        del_dns_view(b1ddi, id)
 
 
 def get_dns_view(b1ddi):
@@ -27,8 +44,8 @@ def get_dns_view(b1ddi):
     if response.status_code == 200:
         dnsView = response.json()
         ip_space_dns = ""
-        ip_spaces_dns = []
         for x in dnsView["results"]:
+            ip_spaces_dns = []
             if x["ip_spaces"]:
                 for i in x["ip_spaces"]:
                     id = i.split("/")
@@ -42,6 +59,23 @@ def get_dns_view(b1ddi):
             )
         console = Console()
         console.print(table)
+    else:
+        print(response.status_code, response.text)
+
+
+def create_dns_view(b1ddi, name, comment, ipspace):
+    dvBody = {"name": name, "comment": comment, "ip_spaces": ipspace}
+    response = b1ddi.create("/dns/view", body=json.dumps(dvBody))
+    if response.status_code == 200:
+        get_dns_view(b1ddi)
+    else:
+        print(response.status_code, response.text)
+
+
+def del_dns_view(b1ddi, id):
+    response = b1ddi.delete("/dns/view", id=id)
+    if response.status_code == 200:
+        get_dns_view(b1ddi)
     else:
         print(response.status_code, response.text)
 
