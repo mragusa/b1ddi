094e72d793d7e3f309c80d2205d79dc3ad023e70
Michael Ragusa <mragusa@users.noreply.github.com>
2025-03-13 03:11:53 -0400

    Changed prettytable with rich tables. Added config option to define file on cli

--- src/b1ddi/bin/b1ztp-join-token.py
+++ src/b1ddi/bin/b1ztp-join-token.py
@@ -2,16 +2,20 @@
 
 import bloxone
 import json
-from prettytable import PrettyTable
 import click
 from click_option_group import optgroup
+from rich.console import Console
+from rich.table import Table
 
 # TODO
-# Change PrettyTable for RichTable
 # Add code to save join token to file
 
 
 @click.command()
+@optgroup.group("BloxOne Configuration")
+@optgroup.option(
+    "--config", default="b1config.ini", help="File containing BloxOne API Key"
+)
 @optgroup.group("BloxOne Zero Touch Provisioning Join Token Actions")
 @optgroup.option(
     "-l", "--list", is_flag=True, default=False, help="List current join tokens"
@@ -31,8 +35,8 @@ from click_option_group import optgroup
 )
 @optgroup.group("B1ZTP ID")
 @optgroup.option("-i", "--id", type=str, help="Join Token ID")
-def main(list, create, delete, registration, id):
-    b1ztp = bloxone.b1ztp("b1config.ini")
+def main(config, list, create, delete, registration, id):
+    b1ztp = bloxone.b1ztp(config)
     if list:
         if id:
             response = b1ztp.get("/jointoken", id=id)
@@ -64,14 +68,15 @@ def main(list, create, delete, registration, id):
                 print(response.status_code, response.text)
         else:
             print("No ID Provided")
+
     if registration:
         federation = input("Federation: True or False")
         if id:
             rToken = {"federation": federation, "join_token": id}
         else:
             joinToken = input("Join Token: ")
-            rToken = {"federation": federation, "join_token": id}
-        response = b1ztp.create("/registration/verify", body=json.dumps(rtoken))
+            rToken = {"federation": federation, "join_token": joinToken}
+        response = b1ztp.create("/registration/verify", body=json.dumps(rToken))
         if response.status_code == 201:
             print("Verification Complete")
         else:
@@ -79,84 +84,81 @@ def main(list, create, delete, registration, id):
 
 
 def list_token(response):
-    table = PrettyTable()
+    table = Table(title="BloxOne Join Tokens")
     joinToken = response
     if "results" in joinToken:
-        table.field_names = [
-            "ID",
-            "Last Used",
-            "Name",
-            "Status",
+        table.add_column("ID", justify="center")
+        table.add_column("Last Used", justify="center")
+        table.add_column("Name", justify="center")
+        table.add_column("Status", justify="center")
+        table.add_column(
             "Token ID",
-            "Use Counter",
-        ]
+            justify="center",
+        )
+        table.add_column("Use Counter", justify="center")
         for x in joinToken["results"]:
             if "last_used_at" in x:
                 table.add_row(
-                    [
-                        x["id"],
-                        x["last_used_at"],
-                        x["name"],
-                        x["status"],
-                        x["token_id"],
-                        x["use_counter"],
-                    ]
+                    x["id"],
+                    x["last_used_at"],
+                    x["name"],
+                    x["status"],
+                    x["token_id"],
+                    str(x["use_counter"]),
                 )
             else:
                 table.add_row(
-                    [
-                        x["id"],
-                        "Unused",
-                        x["name"],
-                        x["status"],
-                        x["token_id"],
-                        x["use_counter"],
-                    ]
+                    x["id"],
+                    "Unused",
+                    x["name"],
+                    x["status"],
+                    x["token_id"],
+                    str(x["use_counter"]),
                 )
     else:
-        table.field_names = [
-            "ID",
-            "Description",
-            "Name",
-            "Status",
+        table.add_column("ID", justify="center")
+        table.add_column("Last Used", justify="center")
+        table.add_column("Name", justify="center")
+        table.add_column("Status", justify="center")
+        table.add_column(
             "Token ID",
-            "Use Counter",
-        ]
-        table.add_row(
-            [
-                joinToken["result"]["id"],
-                joinToken["result"]["description"],
-                joinToken["result"]["name"],
-                joinToken["result"]["status"],
-                joinToken["result"]["token_id"],
-                joinToken["result"]["use_counter"],
-            ]
+            justify="center",
         )
-    print(table)
+        table.add_column("Use Counter", justify="center")
+        table.add_row(
+            joinToken["result"]["id"],
+            joinToken["result"]["description"],
+            joinToken["result"]["name"],
+            joinToken["result"]["status"],
+            joinToken["result"]["token_id"],
+            str(joinToken["result"]["use_counter"]),
+        )
+    console = Console()
+    console.print(table)
 
 
 def create_token(response):
-    table = PrettyTable()
+    table = Table(title="BloxOne Join Tokens")
     createToken = response
-    table.field_names = [
-        "Join Token",
+    table.add_column("ID", justify="center")
+    table.add_column("Last Used", justify="center")
+    table.add_column("Name", justify="center")
+    table.add_column("Status", justify="center")
+    table.add_column(
         "Token ID",
-        "ID",
-        "Name",
-        "Description",
-        "Use Counter",
-    ]
-    table.add_row(
-        [
-            createToken["join_token"],
-            createToken["result"]["token_id"],
-            createToken["result"]["id"],
-            createToken["result"]["name"],
-            createToken["result"]["description"],
-            createToken["result"]["use_counter"],
-        ]
+        justify="center",
     )
-    print(table)
+    table.add_column("Use Counter", justify="center")
+    table.add_row(
+        createToken["join_token"],
+        createToken["result"]["token_id"],
+        createToken["result"]["id"],
+        createToken["result"]["name"],
+        createToken["result"]["description"],
+        str(createToken["result"]["use_counter"]),
+    )
+    console = Console()
+    console.print(table)
 
 
 if __name__ == "__main__":
