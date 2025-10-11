cc91c7500230c9e67b4fed65e25191ab01682a38
Michael Ragusa <mragusa@users.noreply.github.com>
2025-10-11 00:53:24 -0400

    added basic methods for table output and get function. Added basic about section in main()

--- src/b1ddi/bin/b1ddi-framework.py
+++ src/b1ddi/bin/b1ddi-framework.py
@@ -2,6 +2,9 @@
 
 import bloxone
 import click
+from rich.console import Console
+from rich.table import Column, Table
+from rich import box
 
 
 @click.command()
@@ -16,17 +19,47 @@ import click
     show_default=True,
     help="Retrieve DNS views",
 )
-def main(config: str):
+def main(config: str, get: bool):
+    """Example Infoblox UDDI python script"""
     b1 = bloxone.b1ddi(config)
-    get_view(b1)
+    if get:
+        dns_view = get_view(b1)
+        if dns_view:
+            report_dns_view(dns_view)
+        else:
+            print("Unable to retreive UDDI DNS Views")
 
 
 def get_view(b1):
-    b1_subnet = b1.get("/ipam/subnet")
-    if b1_subnet.status_code != 200:
-        print(b1_subnet.status_code, b1_subnet.text)
+    b1_dns_view = b1.get("/dns/view")
+    if b1_dns_view.status_code != 200:
+        print(b1_dns_view.status_code, b1_dns_view.text)
     else:
-        print(b1_subnet.json())
+        return b1_dns_view.json()
+
+
+def report_dns_view(dns_view):
+    table = Table(
+        Column(header="Name", justify="center"),
+        Column(header="ID", justify="center"),
+        Column(header="IP Space", justify="center"),
+        Column(header="Comment", justify="center"),
+        Column(header="Created", justify="center"),
+        Column(header="Updated", justify="center"),
+        title="Infoblox UDDI DNS Views",
+        box=box.SIMPLE,
+    )
+    for v in dns_view["results"]:
+        table.add_row(
+            v["name"],
+            v["id"],
+            str(v["ip_spaces"]),
+            str(v["comment"]),
+            v["created_at"],
+            v["updated_at"],
+        )
+    console = Console()
+    console.print(table)
 
 
 if __name__ == "__main__":
