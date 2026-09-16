fe81f2045606e1c611761bf1422e1d19d5b566e8
Michael Ragusa <mragusa@users.noreply.github.com>
2026-09-17 01:47:38 +0700

    fix: added type flag and updated help display. search for record types and print out the raw json

--- src/b1ddi/bin/b1ddi-dns-record.py
+++ src/b1ddi/bin/b1ddi-dns-record.py
@@ -4,8 +4,8 @@ import bloxone
 import click
 
 
-def get_record(b1):
-    b1_record = b1.get("/dns/record", _limit=1000, _offset=0)
+def get_record(b1, type):
+    b1_record = b1.get("/dns/record", _filter=f'type=="{type}"', _limit=1000, _offset=0)
     if b1_record.status_code != 200:
         print(b1_record.status_code, b1_record.text)
         return 0
@@ -17,21 +17,14 @@ def get_record(b1):
 @click.option(
     "-c", "--config", default="~/b1ddi/b1config.ini", help="bloxone ddi config file"
 )
-@click.option(
-    "-g",
-    "--get",
-    is_flag=True,
-    default=False,
-    show_default=True,
-    help="Retrieve DNS views",
-)
-def main(config: str, get: bool):
+@click.option("-t", "--type", help="UDDI Record Type")
+def main(config: str, type: str):
+    """Search for record type and dump out raw json"""
     b1 = bloxone.b1ddi(config)
-    if get:
-        tenant_records = get_record(b1)
-        if tenant_records:
-            for r in tenant_records:
-                print(r["dns_absolute_name_spec"])
+    tenant_records = get_record(b1, type)
+    if tenant_records:
+        for r in tenant_records:
+            print(r)
 
 
 if __name__ == "__main__":
